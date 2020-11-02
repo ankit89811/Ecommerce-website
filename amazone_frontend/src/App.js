d15b4c24db090656503fb54371050bc9be2f0392
Ankit1999 <chaurasiya1999ankit@gmail.com>
2020-11-02 13:20:16 +0530

    changes

--- amazone_frontend/src/App.js
+++ amazone_frontend/src/App.js
@@ -1,31 +1,31 @@
+
 import React from 'react';
-import Product from './componants/Product';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            Amazone
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <Product key={product._id} product={product}></Product>
-            ))}
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Amazone
+            </a>
           </div>
-        </div>
-      </main>
-      <footer className="row center">Ankit Chaurasiya</footer>
-    </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">Ankit Chaurasiya</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
